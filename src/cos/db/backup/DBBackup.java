c521dae602c29254ca42e187a6a791ad01fdf1ca
WanZheng <wanzheng@gmail.com>
2012-10-23 10:31:10 +0800

    create project

---
+++ src/cos/db/backup/DBBackup.java
@@ -0,0 +1,15 @@
+package cos.db.backup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DBBackup extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
